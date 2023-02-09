c2e622c0e94e7321ced382ca104f830245493d11
Ricardoyjx <138607062@qq.com>
2023-02-09 21:56:00 +0800

    update post requst route

--- cmd/main.go
+++ cmd/main.go
@@ -16,9 +16,26 @@ func main() {
 		c.String(http.StatusOK, "who you are?")
 	})
 
+	r.GET("/query", func(c *gin.Context) {
+		name := c.Param("name")
+		role := c.DefaultQuery("role", "teacher")
+		c.String(http.StatusOK, "%s is a %s", name, role)
+	})
+
 	r.GET("/ask/:name", func(c *gin.Context) {
 		name := c.Param("name")
 		c.String(http.StatusOK, "i am %s !", name)
+	})
+
+	r.POST("/form", func(c *gin.Context) {
+		username := c.PostForm("username")
+		password := c.DefaultPostForm("password", "123456")
+
+		c.JSON(http.StatusOK, gin.H{
+			"username": username,
+			"password": password,
+		})
+
 	})
 
 	r.Run() //default listen and serve on :8080
