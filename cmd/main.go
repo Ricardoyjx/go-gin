2e9cd2e285f66494bd066367c5cdd29c100190d1
Ricardoyjx <138607062@qq.com>
2023-02-09 22:27:48 +0800

    update routes

--- cmd/main.go
+++ cmd/main.go
@@ -27,6 +27,32 @@ func main() {
 		c.String(http.StatusOK, "i am %s !", name)
 	})
 
+	r.POST("/posts", func(c *gin.Context) {
+		id := c.Query("id")
+		page := c.Query("page")
+		username := c.PostForm("username")
+		password := c.PostForm("password")
+
+		c.JSON(http.StatusOK, gin.H{
+			"username": username,
+			"password": password,
+			"id":       id,
+			"page":     page,
+		})
+	})
+
+	//map传参
+	r.POST("/map", func(c *gin.Context) {
+		ids := c.QueryMap("ids")
+		names := c.PostFormMap("names")
+
+		c.JSON(http.StatusOK, gin.H{
+			"ids":   ids,
+			"names": names,
+		})
+
+	})
+
 	r.POST("/form", func(c *gin.Context) {
 		username := c.PostForm("username")
 		password := c.DefaultPostForm("password", "123456")
